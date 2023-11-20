c5f9132b7dcd2b3223de90f11d0a9d7f05b6bb53
David Andersson <david.andersson@zocom.se>
2023-11-20 10:58:54 +0100

    Add project files.

---
+++ ConsoleApp/Car.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+// using motsvarar import
+
+namespace ConsoleApp
+{
+	// en klass motsvarar ett TypeScript-interface
+	// det är en mall som kan användas för att skapa objekt
+	public class Car : ICanDrive
+	{
+		public string Model { get; set; }
+		// Skapa en egenskap med namnet "Model"
+
+		public int WheelCount { get; set; }
+
+
+		private decimal Price { get; set; }
+
+		public void Drive()
+		{
+			Console.WriteLine("Wroooooom");
+		}
+
+		// Synlighetsnivåer: public, protected, private, internal
+	}
+}
---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,45 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ConsoleApp;
+
+Console.WriteLine("Hello, World!");
+
+
+Console.WriteLine("Please input your name: ");
+var input = Console.ReadLine();
+
+Console.WriteLine("Welcome " + input + "!");
+
+int x1 = 42;
+long x2 = 42;
+
+float f1 = 3.14f;
+double f2 = 3.14;
+decimal f3 = 3.14m;
+
+Car kia = new Car()
+{
+    Model = "Kia",
+    WheelCount = 4
+};
+// i JavaScript hade vi gjort så här:
+// let kia = { model: 'Kia', wheelCount: 4 }
+
+kia.WheelCount--;
+//kia.Price = 200.0m;
+// Kan inte komma åt privata egenskaper
+
+
+Peugeot per = new Peugeot(4);
+Console.WriteLine("Märket är: " + per.Model);
+
+// skapa en lista med plats för 5 car-objekt
+Car[] cars = new Car[5];
+
+List<ICanDrive> cars2 = new List<ICanDrive>();
+cars2.Add(kia);
+cars2.Add(per);
+cars2.ForEach(car =>
+{
+    Console.WriteLine("Drive car!");
+    car.Drive();
+});
